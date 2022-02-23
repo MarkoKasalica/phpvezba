ac012a22a49c4c2727789b566603d078d30251b3
MarkoKasalica <97064713+MarkoKasalica@users.noreply.github.com>
2022-02-23 17:52:14 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+
+
+//komentar 1.
+//definisanje varijabli i ubacivanje u tekst koji ide na displej browsera
+        
+        //$name = "Marko";
+
+        //echo "Zdravo moje ime je {$name}";
+
+
+
+
+
+//komentar 2.
+//novi red
+
+       // $age = 50;
+      //  $city = "Belgrade";
+     //   echo  nl2br("Hello, I am {$age} years old \r\n and I live in {$city}");
+
+
+
+
+// komentar 3.
+// varijable mogu da se definisu i van stringa
+
+        $name = "Nikola";
+
+        echo $name . " je bio u prodavnici.";
+
+        // varijabla mora uz sebe da ima tacku (.) kako bi kod funkcionisao ako varijabla ide direktno uz string ili integer ili sta god - onda kod nece funkcionisati
+
+
+
+
+
+
+
+
+
+    ?>
+</body>
+</html>
