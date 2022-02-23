3e93d549d97f6e58a1c317d9bdb1c7f0f7d0d60a
MarkoKasalica <97064713+MarkoKasalica@users.noreply.github.com>
2022-02-23 21:49:58 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+
+
+//komentar 1.
+//definisanje varijabli i ubacivanje u tekst koji ide na displej browsera
+        
+        //$name = "Marko";
+
+        //echo "Zdravo moje ime je {$name}";
+
+
+
+
+
+//komentar 2.
+//novi red
+
+       // $age = 50;
+      //  $city = "Belgrade";
+     //   echo  nl2br("Hello, I am {$age} years old \r\n and I live in {$city}");
+
+
+
+
+// komentar 3.
+// varijable mogu da se definisu i van stringa
+
+      //  $name = "Nikola";
+
+      //  echo $name . " je bio u prodavnici.";
+
+        // varijabla mora uz sebe da ima tacku (.) kako bi kod funkcionisao ako varijabla ide direktno uz string ili integer ili sta god - onda kod nece funkcionisati
+
+
+//komentar 4.
+// kreiranje arrays-a
+// vadjenje index-a [] - kao i JScript - nula(0) je pocetni broj 
+
+
+                // $cars1 = "BMW";
+                // $cars2 = "Audi";
+                // $cars3 = "Mercedes";
+
+                // $cars = array("BMW", "Audi", "Mercedes");
+
+                // echo $cars[2];
+
+
+//komentar 5. 
+//var_dump - Dumps information about a variable
+//print_r — Prints human-readable information about a variable
+
+           // $myCar = array("Audi", 2015, 75.304);
+           // var_dump($myCar);
+
+            //echo "<br>";
+
+
+           // $cars = array("BMW", "Audi", "Mercedes");
+           // print_r ($cars);
+
+
+
+//komentar 6.
+//array_merge — Merge one or more arrays
+
+
+            //$food1 = array("apples", "eggs");
+            
+            //print_r($food1);
+         
+            //echo "<br>";
+
+            //$food2 = array("burek", "cheese");
+
+           // print_r($food2);
+
+
+
+            //$food = array_merge($food1, $food2);
+            //ovde su spojena dva array-a - koristili smo array_merge funkciju koja spaja array-a
+            //print_r ($food);
+
+
+
+// komentar 7.
+//
+
+
+    ?>
+</body>
+</html>
---
+++ superglobals.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+                // //Superglobals
+                // //GLOBALS
+                // $_POST
+                // $_GET
+                // $_SERVER
+                // $_COOKIE
+                // $_SESSION
+                // $_FILES
+
+
+                
+    
+    ?>
+</body>
+</html>
